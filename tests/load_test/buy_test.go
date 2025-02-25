a74f6f0c2b259bb548c9159894ecf4ea1ff00612
George <epishcom@gmail.com>
2025-02-25 13:31:26 +0300

    registered users in main func

--- tests/load_test/buy_test.go
+++ tests/load_test/buy_test.go
@@ -5,18 +5,15 @@ package load_test
 import (
 	"encoding/json"
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	vegeta "github.com/tsenart/vegeta/v12/lib"
 	"golang.org/x/exp/rand"
-	"net/http"
-	"strings"
-	"sync"
+	"os"
 	"testing"
 	"time"
 )
 
 func TestBuyLoad(t *testing.T) {
-	tokens, err := registerUsers(100000)
+	tokens, err := loadTokensFromFile("./register_users/tokens.json")
 	if err != nil {
 		t.Fatalf("Failed to register users: %v", err)
 	}
@@ -73,84 +70,16 @@ func TestBuyLoad(t *testing.T) {
 
 }
 
-func generateUserList(count int) []string {
-	users := make([]string, 0, count)
-	for i := 0; i < count; i++ {
-		users = append(users, fmt.Sprintf("user_%d", i+1))
+func loadTokensFromFile(filename string) ([]string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("Ошибка чтения файла с токенами: %w", err)
 	}
 
-	return users
-}
-
-func registerUsers(count int) ([]string, error) {
-	timeStart := time.Now()
-	users := generateUserList(count)
-	tokens := make([]string, 0, count)
-	var mu sync.Mutex
-
-	// количество горутин, отправляющих запрросы
-	workerLimit := 100
-	sem := make(chan struct{}, workerLimit)
-	errCh := make(chan error, count)
-	var wg sync.WaitGroup
-
-	for idx, user := range users {
-		sem <- struct{}{}
-		wg.Add(1)
-
-		go func(user string, idx int) {
-			defer wg.Done()
-			defer func() { <-sem }()
-
-			token, err := registerUser(user)
-			if err != nil {
-				errCh <- err
-				return
-			}
-
-			mu.Lock()
-			tokens = append(tokens, token)
-			mu.Unlock()
-
-			if idx > 0 && idx%500 == 0 {
-				log.Infof("registered: %d users", idx)
-			}
-		}(user, idx)
+	var tokens []string
+	if err = json.Unmarshal(data, &tokens); err != nil {
+		return nil, fmt.Errorf("Ошибка десериализации токенов: %w", err)
 	}
-
-	wg.Wait()
-	close(errCh)
-
-	if len(errCh) > 0 {
-		return nil, <-errCh
-	}
-
-	log.Printf("Общее время регистрации пользователей: %v", time.Now().Sub(timeStart))
 
 	return tokens, nil
-}
-
-func registerUser(user string) (string, error) {
-	resp, err := http.Post(
-		"http://localhost:8080/api/auth",
-		"application/json",
-		strings.NewReader(fmt.Sprintf(`{"username": "%s", "password": "test_pass"}`, user)),
-	)
-	if err != nil {
-		return "", fmt.Errorf("failed to register user %s: %w", user, err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("failed to register user %s: status %d", user, resp.StatusCode)
-	}
-
-	var result struct {
-		Token string `json:"token"`
-	}
-	if err = json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return "", fmt.Errorf("failed to decode response for user %s: %w", user, err)
-	}
-
-	return result.Token, nil
 }
